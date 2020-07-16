8f21d1848667689dd6bdea9e0dc38aca8a70d4de
giribabu.n <giribabu.n@iaaatech.com>
2020-07-16 21:37:06 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,99 @@
 import { Component } from '@angular/core';
-
+import { trigger, transition, style, animate, query, stagger} from '@angular/animations';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('itemAnim', [
+      // ENTRY ANIMATION
+      transition('void => *', [
+        // Initial state
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.85)',
+          'margin-bottom': 0,
+
+          // we have to 'expand' out the padding properties
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+        }),
+        // we first want to animate the spacing (which includes height and margin)
+        animate('50ms', style({
+          height: '*',
+          'margin-bottom': '*',
+          paddingTop: '*',
+          paddingBottom: '*',
+          paddingLeft: '*',
+          paddingRight: '*',
+        })),
+        animate(68)
+      ]),
+
+      transition('* => void', [
+        // first scale up
+        animate(50, style({
+          transform: 'scale(1.05)'
+        })),
+        // then scale down back to normal size while beginning to fade out
+        animate(50, style({
+          transform: 'scale(1)',
+          opacity: 0.75
+        })),
+        // scale down and fade out completely
+        animate('120ms ease-out', style({
+          transform: 'scale(0.68)',
+          opacity: 0,
+        })),
+        // then animate the spacing (which includes height, margin and padding)
+        animate('150ms ease-out', style({
+          height: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+          'margin-bottom': '0',
+        }))
+      ])
+    ]),
+
+    trigger('listAnim', [
+      transition('* => *', [
+        query(':enter', [
+          style({
+            opacity: 0,
+            height: 0
+          }),
+          stagger(100, [
+            animate('0.2s ease')
+          ])
+        ], {
+          optional: true
+        })
+      ])
+    ])
+  ]
 })
+
 export class AppComponent {
-  title = 'angularAnimations';
+  listItem = [
+    {id: 1, name:'Superman'},
+    {id: 2, name:'Batman'},
+    {id: 5, name:'BatGirl'},
+    {id: 3, name:'Robin'},
+    {id: 4, name:'Flash'}
+  ]
+
+  deleteNote(hero){
+    this.listItem.splice(this.listItem.indexOf(hero), 1);
+  }
+
+  ngOnInit(){
+    AOS.init();
+  }
+ 
 }
